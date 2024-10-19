07ad6c40f1baee9a414d8586b92f3dfe24e88449
228aboba <164630385+228aboba@users.noreply.github.com>
2024-10-19 17:55:46 +0300

    Update markups.py

--- markups/markups.py
+++ markups/markups.py
@@ -9,7 +9,7 @@ pr2iv21et2Button = KeyboardButton('Отправить заявку 📃')
 mainButton.add(pr2iv21et2Button, pri3v2et2Button)
 
 # создание кнопок Inline
-# mainInlineButtons = InlineKeyboardMarkup()
+mainInlineButtons = InlineKeyboardMarkup()
 ExitButtonInline = InlineKeyboardButton('Назад' , callback_data='exit')
 KlientButtonInline = InlineKeyboardButton('Записаться' , callback_data='Klient')
-
+mainInlineButtons.add(KlientButtonInline, ExitButtonInline)
